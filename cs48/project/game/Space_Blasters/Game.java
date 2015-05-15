2d86fd346020ca5e10556918dc9db20c52f15800
Richard Alvarez <ralvarez@umail.ucsb.edu>
2015-05-15 01:04:30 -0700

    Changed to standard main call

--- cs48/project/game/Space_Blasters/Game.java
+++ cs48/project/game/Space_Blasters/Game.java
@@ -407,9 +407,9 @@ public class Game extends Canvas {
      * instance of class which will start the display and game
      * loop.
      *
-     * @param argv The arguments that are passed into our game
+     * @param args The arguments that are passed into our game
      */
-    public static void main(String argv[]) {
+    public static void main(String args[]) {
 
         Game g = new Game();
         //new Thread(new GameMusic()).start();
