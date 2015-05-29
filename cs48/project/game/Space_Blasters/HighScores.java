d656aa12ef273c2ce3ecbaea440f290f5b5ee6fe
Vivek <vivek_patel@umail.ucsb.edu>
2015-05-28 20:37:56 -0700

    Added mongodb jar to lib/ package and added code to connect to database and database collection (WIP).

--- cs48/project/game/Space_Blasters/HighScores.java
+++ cs48/project/game/Space_Blasters/HighScores.java
@@ -4,90 +4,56 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Graphics;
 import java.io.*;
-import java.sql.*;
 import java.util.*;
+import com.mongodb.*;
+import com.mongodb.Cursor;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Created by Vivek on 5/7/2015.
  */
 public class HighScores extends JPanel {
 
-    private ArrayList<String> names;
-    private ArrayList<Long> scores;
+    private long[] scoreList;
     private String[][] tableData;
     private JLabel scoreLabel;
+    private MongoDatabase db;
+    MongoCollection highScores;
+    private JTable scoreTable;
 
     /**
      * Default no-arg constructor for HighScore JPanel
      */
     public HighScores() {
         super();
-        names = new ArrayList<String>();
-        scores = new ArrayList<Long>();
+        connectToDatabase();
+        scoreList = new long[10];
         tableData = new String[10][2];
-        readFile();
-        String[] columnNames = {"Names", "Scores"};
-        JTable scoreTable = new JTable(tableData, columnNames);
-        scoreTable.setPreferredScrollableViewportSize(new Dimension(500, 70));
-        scoreTable.setFillsViewportHeight(true);
+        fillScoreTable();
         this.add(scoreTable, BorderLayout.CENTER);
     }
 
-    private void readFile() {
-        String fileName = "res/highscores.txt";
-        String line = null;
+    private void connectToDatabase() {
         try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader = new FileReader(new File(fileName));
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            int row = 0;
-            int column;
-            while ((line = bufferedReader.readLine()) != null) {
-                column = 0;
-                String[] result = line.split(" ");
-                names.add(result[0]);
-                scores.add(Long.parseLong(result[1]));
-                tableData[row][column] = result[0];
-                tableData[row][column + 1] = result[1];
-                row++;
-            }
-            // Always close files.
-            bufferedReader.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + fileName + "'");
-        } catch (IOException ex) {
+            MongoClientURI uri = new MongoClientURI("mongodb://vivek_patel:root@ds034348.mongolab.com:34348/spaceblasters");
+            MongoClient mongoClient = new MongoClient(uri);
+            mongoClient.getDatabase("spaceblasters");
+            db = mongoClient.getDatabase("spaceblasters");
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-}
 
-/*
-    Not working right now! WIP
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/";
-        String dbName = "demo";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "root";
-        String password = "";
-        try {
-            Class.forName(driver).newInstance();
-            Connection conn = DriverManager.getConnection(url + dbName, userName, password);
-            Statement st = conn.createStatement();
-            ResultSet res = st.executeQuery("SELECT * FROM event");
-            while (res.next()) {
-                int id = res.getInt("id");
-                String msg = res.getString("msg");
-                System.out.println(id + "\t" + msg);
-            }
-            int val = st.executeUpdate("INSERT into event VALUES(" + 1 + "," + "'Easy'" + ")");
-            if (val == 1) System.out.print("Successfully inserted value");
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void fillScoreTable() {
+        highScores = db.getCollection("HighScores");
+
     }
 
-*/
+    public void addHighScore(String name, long score) {
+
+    }
+}
 
 
